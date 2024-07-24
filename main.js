176e7616b447bd0693dcb6546c9750a9f85a97f0
Julia Bosianek <frontend.julia@gmail.com>
2024-07-24 16:32:51 +0200

    normalize and reset CSS added

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const firstName = 'Julia';
 
 console.log(`zesc! Nazywam sie ${firstName}. Milo Cie widziec :D`);
 alert(`Czesc! Nazywam sie ${firstName}. Milo Cie widziec :D`);
+
+const header = document.querySelector('h1')
+
+console.log(header)
