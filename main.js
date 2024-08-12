3644b15f6ef95761e320d80fe9c3bb2ee6b0f616
Julia Bosianek <frontend.julia@gmail.com>
2024-08-12 08:31:26 +0200

    homework week no 4
    applying BEM method, styling width and center of the content, adding JS- innerHtml to add content in an article

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const firstName = 'Julia';
 
-console.log(`zesc! Nazywam sie ${firstName}. Milo Cie widziec :D`);
+console.log(`Czesc! Nazywam sie ${firstName}. Milo Cie widziec :D`);
 alert(`Czesc! Nazywam sie ${firstName}. Milo Cie widziec :D`);
 
 const header = document.querySelector('h1')
 
 console.log(header)
+
+const contentJs = document.querySelector('.my-article__paragraph--js');
+contentJs.innerHTML = 'Jestem wypelniony javascriptem 🤯'
